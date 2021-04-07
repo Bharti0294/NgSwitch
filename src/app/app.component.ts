b389ce04f55e96b6bef08b87ed4f2273012d452f
Bharti0294 <bharti.nehul07@gmail.com>
2021-04-06 23:52:34 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+  
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'ngSwitch';
+  title = 'switch';
+
+  people: any[] = [
+    {
+      "name": "Ganesh Devadhe",
+      "age": 33,
+      "country": 'Ensono'
+    },
+    {
+      "name": "Varsha Desai",
+      "age": 27,
+      "country": 'Dista'
+    },
+    {
+      "name": "Rinky sen",
+      "age": 29,
+      "country": 'wipro'
+    },
+    {
+      "name": "Pradnya Garud",
+      "age": 29,
+      "country": 'Ensono'
+    },
+    {
+      "name": "Nikhil Kumbhar",
+      "age": 32,
+      "country": 'Dista'
+    }
+  ];
 }
